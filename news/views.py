8bceb6da28d3da96b0f218f5cd077607efd6b229
Cencious <user.email>
2022-05-23 18:19:10 +0300

    create a string and dispay day, month and year

--- news/views.py
+++ news/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse #responsible for returning a response to a user. 
+import datetime as dt
 
 # Create your views here.
 def welcome(request): 
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def news_of_day(request):
+    date=dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+    return HttpResponse(html)
+    '''
