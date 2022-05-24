5a554e9c7a4608b2cd7c48b5862a29ed8b09f8ae
Cencious <user.email>
2022-05-25 01:10:30 +0300

    configure urls

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views 
 
-
+#URLConfiguration
 urlpatterns =[
     path('',views.welcome,name='welcome'),
-    path('',views.news_of_day,name='newsToday')]
+    path('today/',views.news_of_day,name='newsToday'),
+    path('archives/(\d{4}-\d{2}-\d{2})/', views.past_days_news, name='pastnews')
+    
+    ]
