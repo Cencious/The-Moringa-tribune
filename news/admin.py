a78a6b4911995c4ec44b281aa9a98bc6c01509f1
Cencious <user.email>
2022-05-27 08:56:16 +0300

    sort bugs

--- news/admin.py
+++ news/admin.py
@@ -4,7 +4,7 @@ from .models import Editor,Article, tags
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    filter_horizontal:('tags')
+    filter_horizontal=('tags',)
     
 admin.site.register(Editor)
 admin.site.register(Article,ArticleAdmin)
