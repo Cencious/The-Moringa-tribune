9f1616bc586cb5064dcff75c73a6157830e25fb5
Cencious <user.email>
2022-06-01 01:32:59 +0300

    configure email function to connect to the environment variables

--- tribune/settings.py
+++ tribune/settings.py
@@ -133,6 +133,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
