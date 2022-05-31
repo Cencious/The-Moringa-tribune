c1723b0702808bfd4df3ff6b2d06c8a844c8f640
Cencious <user.email>
2022-06-01 01:39:24 +0300

    create the EmailMultiAlternativesobject and pass in the subject, the text email template the sender and the receiver.

--- news/email.py
+++ news/email.py
@@ -9,3 +9,7 @@ def send_welcome_email(name,receiver):
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name": name})
     html_content = render_to_string('email/newsemail.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
